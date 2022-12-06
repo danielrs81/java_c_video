6a67c43486ec25a06649c586b701f8f6e1e97edb
Daniel Rodrigues da Silva <115389640+danielrs81@users.noreply.github.com>
2022-12-06 19:24:56 -0300

    up

--- CursoJS/aula12/ex015_novo/script/script.js
+++ CursoJS/aula12/ex015_novo/script/script.js
@@ -8,18 +8,30 @@ function verificar() {
     //res.innerHTML = `${idade}`
     let formuSexo = window.document.getElementsByName('sexo')
     let genero = ''
+
+    let img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+
+    if (anoNascimento == 0 || anoNascimento > atual) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    }
     if (formuSexo[0].checked) {
         genero = 'Masculino'
         if (idade >= 0 && idade < 4) {
             res.innerHTML = `Você é um bebê do sexo masculino com ${idade} anos de idade`
+            img.setAttribute('src', './imagens/bebe_h.png')
         } else if (idade < 10) {
             res.innerHTML = `Você é uma criança do sexo masculino com ${idade} anos de idade`
+            img.setAttribute('src', './imagens/crianca_h.png')
         } else if (idade < 18) {
             res.innerHTML = `Você é um jovem do sexo masculino com ${idade} anos de idade`
-        } else if (idade < 45) {
+            img.setAttribute('src', './imagens/jovem_h.png')
+        } else if (idade < 50) {
             res.innerHTML = `Você é um adulto do sexo masculino com ${idade} anos de idade`
+            img.setAttribute('src', './imagens/adulto_h.png')
         } else {
             res.innerHTML = `Você é um idoso do sexo masculino com ${idade} anos de idade`
+            img.setAttribute('src', './imagens/idoso_h.png')
         }
     }
 
@@ -27,17 +39,24 @@ function verificar() {
         genero = 'Feminino'
         if (idade >= 0 && idade < 4) {
             res.innerHTML = `Você é um bebê do sexo feminino com ${idade} anos de idade`
+            img.setAttribute('src', './imagens/bebe_m.png')
         } else if (idade < 10) {
             res.innerHTML = `Você é uma criança do sexo feminino com ${idade} anos de idade`
+            img.setAttribute('src', './imagens/crianca_m.png')
         } else if (idade < 18) {
             res.innerHTML = `Você é um jovem do sexo feminino com ${idade} anos de idade`
-        } else if (idade < 45) {
+            img.setAttribute('src', './imagens/jovem_m.png')
+        } else if (idade < 50) {
             res.innerHTML = `Você é um adulto do sexo feminino com ${idade} anos de idade`
+            img.setAttribute('src', './imagens/adulto_m.png')
         } else {
             res.innerHTML = `Você é um idoso do sexo feminino com ${idade} anos de idade`
+            img.setAttribute('src', './imagens/idoso_m.png')
         }
     }
 
+    res.style.textAlign = 'center'
+    res.appendChild(img)
     //res.innerHTML += genero
    
     
