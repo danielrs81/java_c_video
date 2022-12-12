b3935701a9888f4f8abc6dfc814d18bc03cef502
Daniel Rodrigues da Silva <115389640+danielrs81@users.noreply.github.com>
2022-12-12 17:43:06 -0300

    up

--- CursoJS/aula16ex/ex018/script/script.js
+++ CursoJS/aula16ex/ex018/script/script.js
@@ -26,15 +26,17 @@ function inLista(n, l) {
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
         //window.alert('Tudo Ok')
-        valores.push(Number(num.value))
-        let item = document.createElement('option')
+        valores.push(Number(num.value)) // ADICIONAR UM NUMERO NA LISTA
+        let item = document.createElement('option') // CRIAR O NR NA LISTA VISUALMENTE, CRIAR UM OPTION
         item.text = `Valor ${num.value} adcionado.`
         lista.appendChild(item)
+        res.innerHTML = ""
+
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
-    num.value = ""
-    num.focus()
+    num.value = "" // Apagar o numero após inserido na lista
+    num.focus()  // Manter o cursor no input adicionar
 }
 
 
@@ -43,8 +45,27 @@ function finalizar() {
         window.alert('Adicione valores antes de finalizar')
     } else {
         let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            // media = soma / valores.length // funciona tambem
+            if(valores[pos] > maior)
+            maior = valores[pos]
+            if(valores[pos] < menor)
+            menor = valores[pos]
+        }
+
+        
+        media = soma / tot
         res.innerHTML = ""
         res.innerHTML += `<p>Ao todo, temos ${tot} número cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>A soma de todos os valores é ${soma}</p>`
+        res.innerHTML += `<p>A media de todos os valores é ${media}</p>`
     }
     
 }
